745c255df4a4689fcbd3ec144b90be7b2944dd3d
Trent Oliphant <trentoliphant@gmail.com>
2012-12-02 11:36:36 -0700

    Create basic pages (about, home, press, etc)

--- code/pydata/events/models.py
+++ code/pydata/events/models.py
@@ -1,3 +1,19 @@
+import datetime
 from django.db import models
 
-# Create your models here.
+# Create your models here
+class Conference(models.Model):
+    class Meta:
+        verbose_name = _('Conference')
+        verbose_name_plural = _('Conferences')
+
+    name = models.CharField(max_length = 15)
+    title = models.CharField(max_length = 200)
+    location = models.CharField(max_length = 100)
+    begin_date = models.DateTimeField('Start Date')
+    end_date = models.DateTimeField('End Date')
+
+    def __unicode__(self):
+        return self.name
+
+   
