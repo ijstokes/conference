5f9b1e04d5f6cc32f9d334cf74f2d1c7a8aef586
Trent Oliphant <trentoliphant@gmail.com>
2012-12-10 23:47:31 -0700

    cleanup ROOT_PATH in settings.py

--- code/pydata/utilities/utilities.py
+++ code/pydata/utilities/utilities.py
@@ -2,22 +2,22 @@ from os import popen
 
 MAIL = '/usr/sbin/sendmail'
 
-def send_plain_email(recipient, message, sender = 'admin@pydata.org', subject = 'Automated Email from PyData.org'):
-	template = """
-To: {0}
+
+def send_plain_email(recipient, message, sender='admin@pydata.org', subject='Automated Email from PyData.org'):
+    template = """To: {0}
 From: {1}
 Subject: {2}
 
 {3}
 
 """
-	send_string = template.format(recipient, sender, subject, message)
+    send_string = template.format(recipient, sender, subject, message)
 
-	print send_string
+    print send_string
 
-	p = popen(MAIL + ' -t', 'w')
-	p.write(send_string)
-	exitcode = p.close()
+    p = popen(MAIL + ' -t', 'w')
+    p.write(send_string)
+    exitcode = p.close()
 
-	return exitcode
+    return exitcode
 
