ccfab8ba2a4d90ee3e5c9355c914260446d9f621
Trent Oliphant <trentoliphant@gmail.com>
2013-01-24 14:12:41 -0700

    Include sponsors in abstracts and bios

--- code/pydata/speakers/views.py
+++ code/pydata/speakers/views.py
@@ -1,6 +1,7 @@
 from speakers.models import Speaker, Presentation
 from django.shortcuts import render_to_response
 from utilities.utilities import get_base_out_vars
+from sponsors.models import SponsorLevel
 # Create your views here.
 
 
@@ -8,6 +9,7 @@ def view_speakers(request, **kwargs):
     output = get_base_out_vars(request, **kwargs)
     speakers = Speaker.objects.exclude(presentation__scheduleditem__itemType__name='Keynote')
     output['speakers'] = speakers.exclude(name="TBD")
+    output['levels'] = SponsorLevel.objects.filter(conference__exact=1)
 
     return render_to_response('sv2013/templates/speaking/bios.html', output)
 
@@ -16,5 +18,6 @@ def view_abstracts(request, **kwargs):
     output = get_base_out_vars(request, **kwargs)
     abstracts = Presentation.objects.exclude(scheduleditem__itemType__name='Keynote')
     output['abstracts'] = abstracts.exclude(title="Waiting for confirmation.")
+    output['levels'] = SponsorLevel.objects.filter(conference__exact=1)
 
     return render_to_response('sv2013/templates/speaking/abstracts.html', output)
