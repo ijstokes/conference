01b6f1f9830b7503dbf571a5bce24da1cc236832
Ian Stokes-Rees <ijstokes@continuum.io>
2013-05-22 17:31:50 -0400

    Subtree merged in debug_toolbar

---
+++ code/debug_toolbar/tests/views.py
@@ -0,0 +1,12 @@
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+
+def execute_sql(request):
+    list(User.objects.all())
+    
+    return HttpResponse()
+
+def resolving_view(request, arg1, arg2):
+    # see test_url_resolving in tests.py
+    return HttpResponse()
+
