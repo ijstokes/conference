317339d65bfa797b90ffd928d584818f6b5f7dcb
Troy Powell <tpowell@continuum.io>
2013-05-29 23:49:08 +0000

    removing toolbar and pushing db updates

--- code/pydata/pydata/settings.py
+++ code/pydata/pydata/settings.py
@@ -118,7 +118,7 @@ DEBUG_TOOLBAR_PANELS = (
 )
 
 def custom_show_toolbar(request):
-    return True  # Always show toolbar, for example purposes only.
+    return False  # Always show toolbar, for example purposes only.
 
 DEBUG_TOOLBAR_CONFIG = {
     'INTERCEPT_REDIRECTS': False,
@@ -130,7 +130,7 @@ DEBUG_TOOLBAR_CONFIG = {
 
 # Hosts/domain names that are valid for this site; required if DEBUG is False
 # See https://docs.djangoproject.com/en/1.5/ref/settings/#allowed-hosts
-ALLOWED_HOSTS = ['localhost']
+ALLOWED_HOSTS = ['127.0.0.1']
 
 INTERNAL_IPS = ('127.0.0.1',)
 
--- code/pydata/start.sh
+++ code/pydata/start.sh
@@ -3,9 +3,9 @@
 set -e
 LOGFILE=error.log
 NUM_WORKERS=3
-BIND=127.0.0.1:8013
+BIND=50.56.188.141.1:8000
 # user/group to run as
 exec gunicorn_django -w $NUM_WORKERS \
-	--bind=127.0.0.1:8013 \
+	--bind=50.56.188.141:8000 \
     --log-level=debug \
 	--log-file=error.log 2>> error.log &
